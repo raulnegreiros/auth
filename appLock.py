c9211c672b94aa8abf6c56441f16278270ac307c
Eric Baum <ebaum@cpqd.com.br>
2018-01-17 15:03:42 -0200

    Initialization code cleanup

--- appLock.py
+++ appLock.py
@@ -1,59 +1,55 @@
 #!/usr/bin/python3
 import postgresql
-import sys
+from postgresql import exceptions
 from time import sleep
+from argparse import ArgumentParser
 
 import auth.conf as dbconf
 
-poolingsec = 5
-max_retries = 10
-retries = 0
 
-
-def incRetries():
-    global retries
-    retries = retries + 1
-    if retries >= max_retries:
-        print("max_retries reached. Giving up...")
-        exit(-1)
-
-
-def waitPostgres():
+def wait_postgres(polling_sec, max_retries):
     print('Waiting for postgres...')
+
     retries = 0
-    while (True):
+    while retries < max_retries:
         try:
             db = postgresql.open('pq://'
                                  + dbconf.dbUser
                                  + ':' + dbconf.dbPdw
                                  + '@' + dbconf.dbHost)
             if db is not None:
-                break
-        except postgresql.exceptions.ClientCannotConnectError as e:
+                print('Postgres is ready')
+                return 0
+
+        except exceptions.ClientCannotConnectError as e:
             print("Failed to connect to database. Error:" + str(e))
 
-        incRetries()
-        print('Will try again in ' + str(poolingsec))
-        sleep(poolingsec)
-    print('Postgres is ready')
+        retries = retries + 1
+        print('Will try again in ' + str(polling_sec))
+        sleep(polling_sec)
+
+    print("Max retries reached. Giving up...")
+    return 1
 
 
-def verifyParams():
-    i = 1
-    while i < len(sys.argv):
-        if sys.argv[i] == '--sleep':
-            global poolingsec
-            poolingsec = int(sys.argv[i+1])
-            i = i + 1
-        if sys.argv[i] == '--max_retries':
-            global max_retries
-            max_retries = int(sys.argv[i+1])
-            i = i + 1
+def verify_params():
 
-        i = i + 1
+    parser = ArgumentParser()
+
+    # Add more options if you like
+    parser.add_argument("-s", "--sleep", dest="polling_sec", action="store", default=5,
+                        help="Retries interval in seconds", type=int)
+
+    parser.add_argument("-r", "--max_retries", dest="max_retries",
+                        action="store", default=10,
+                        help="Maximum number of retries", type=int)
+
+    args = parser.parse_args()
+
+    return args
 
 
 if __name__ == '__main__':
-    verifyParams()
-    waitPostgres()
-    exit(0)
+    params = verify_params()
+    ret = wait_postgres(params.polling_sec, params.max_retries)
+    exit(ret)
